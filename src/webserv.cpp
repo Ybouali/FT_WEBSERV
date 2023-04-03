0f1e8740e75dd222c2d6afb278d6f72dee4a4a2a
ybouali <yassine.bouali.bo@gmail.com>
2023-04-03 20:37:30 +0000

    DATA NEEDED FROM THE CONFIG FILE

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -2,7 +2,22 @@
 
 int main(int ac, char **av)
 {
-    
+    // ! WHAT NEEDS BY THE SERVERS FROM THE CONFIG FILE
 
+    // ! Create A Class that contains this variables
+    // ! and it should be a vector of this class SO that means every servers will be a object of this class
+    /*
+        uint16_t						Port;
+		in_addr_t						Host;
+		std::string						Server_name;
+		std::string						Root;
+		unsigned long					clientMaxBodySize;
+		std::string						index;
+		bool							autoindex;
+		std::map<short, std::string>	errorPages;
+		std::vector<??> 			    locations;
+        struct sockaddr_in 				serverAddress;
+        int     						listenFd;
+    */
     return 0;
 }
